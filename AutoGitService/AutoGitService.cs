17741eb5ce04bb350473ce0fa3e2ba6ec39e4dfa
m978718484 <m978718484@163.com>
2017-09-22 11:22:00 +0800

    'init'

---
+++ AutoGitService/AutoGitService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Timers;
+using System.IO;
+
+namespace AutoGitService
+{
+    public partial class AutoGitService : ServiceBase
+    {
+        public AutoGitService()
+        {
+            InitializeComponent();
+        }
+        string filePath = "F:\\temp.txt";
+        static int i = 0;
+        Timer _timer = new Timer();
+
+        protected override void OnStart(string[] args)
+        {
+            _timer.AutoReset = true;
+            _timer.Interval = 2000;
+            _timer.Elapsed += new ElapsedEventHandler(_timer_Elapsed);
+            _timer.Start();
+        }
+
+        void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Stream fs = File.OpenWrite(filePath);
+            TextWriter sw = new StreamWriter(fs);
+            i++;
+            sw.WriteLine(i.ToString() + ", hi, this is my first Services");
+            sw.Flush();
+            sw.Close();
+        }
+
+        protected override void OnStop()
+        {
+            Stream fs = File.OpenWrite(filePath);
+            TextWriter sw = new StreamWriter(fs);
+            sw.WriteLine("panel !!!!!!!!!!!!!!!");
+            sw.Flush();
+            sw.Close();
+        }
+    }
+}
---
+++ AutoGitService/bin/Debug/install.bat
@@ -0,0 +1,1 @@
+installutil AutoGitService.exe
